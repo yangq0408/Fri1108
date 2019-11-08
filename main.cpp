d4d8341c102bb88edb0da2acc3023819c616426f
Yang Gyuwon <rbdnjs95@snu.ac.kr>
2019-11-08 15:54:11 +0900

    d Signed-off-by: Yang Gyuwon <rbdnjs95@snu.ac.kr>

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void renderScene()
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();	
-	gluPerspective(30.0f, 1.0f, 0.1f, 100.0f);
+	gluPerspective(60.0f, 1.0f, 0.1f, 100.0f);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
